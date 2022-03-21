897f56e08584a2499fe54dde45a1ac299c61b5db
Harsha <harshavardhan.achyut@gmail.com>
2022-03-21 13:08:41 +0100

    Done

--- testing.py
+++ testing.py
@@ -2,6 +2,13 @@ import pandas as pd
 from statsmodels.graphics.tsaplots import plot_pacf
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('merged_data/train.csv')
-plot_pacf(df['travel_time'])
+#df = pd.read_csv('merged_data/train.csv')
+#plot_pacf(df['travel_time'])
+
+def fun():
+    if 2 == 2:
+        return 2
+
+
+print(fun())
 
